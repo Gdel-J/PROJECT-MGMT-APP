40a60a17aba8bdfd6357e6a2a8b5dd2713acb736
Gdel-J <jessygdelvecchio@gmail.com>
2023-09-21 20:26:27 -0400

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,55 @@
+
+const { project, clients } = require('../server/sampleData')
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList }= require('graphql')
+
+//Project Type
+
+const ProjectType = new GraphQLObjectType({
+    name:'Client',
+    fields:() =>({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { typeGraphQLString },
+        client:{
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find(client =>client.id ===parent.clientId)
+            }
+        }
+    })
+});
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+    name:'Client',
+    fields:() =>({
+        id: { type: GraphQLID },
+        name:  { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: { 
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            }
+        },
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return clients.find(client => client.id === args.id);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
---
+++ server/sampleData.js
@@ -0,0 +1,81 @@
+// Projects
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'Mercury',
+    description:
+      'Mercury is the smallest and innermost planet in our Solar System. It orbits the Sun once every 88 days and has no moons.',
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Venus',
+    description:
+      'Venus is the second planet from the Sun and is known for its intense heat and acidic atmospheric conditions. It has no moons.',
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'Earth',
+    description:
+      'Earth is the third planet from the Sun and the only one known to support life. It has one moon called Luna.',
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Mars',
+    description:
+      'Mars is the fourth planet from the Sun and is often referred to as the "Red Planet" because of its reddish appearance. It has two moons: Phobos and Deimos.',
+    status: 'Done',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Jupiter',
+    description:
+      'Jupiter is the largest planet in our Solar System and is primarily composed of gas. It has a strong magnetic field and over 79 known moons.',
+    status: 'In Progress',
+  },
+];
+
+// Clients
+// (No changes made to the Clients array as per the requirements)
+
+const clients = [
+  {
+    id: '1',
+    name: 'Tony Stark',
+    email: 'ironman@gmail.com',
+    phone: '343-567-4333',
+  },
+  {
+    id: '2',
+    name: 'Natasha Romanova',
+    email: 'blackwidow@gmail.com',
+    phone: '223-567-3322',
+  },
+  {
+    id: '3',
+    name: 'Thor Odinson',
+    email: 'thor@gmail.com',
+    phone: '324-331-4333',
+  },
+  {
+    id: '4',
+    name: 'Steve Rogers',
+    email: 'steve@gmail.com',
+    phone: '344-562-6787',
+  },
+  {
+    id: '5',
+    name: 'Bruce Banner',
+    email: 'bruce@gmail.com',
+    phone: '321-468-8887',
+  },
+];
+
+module.exports = { projects, clients };
