231c698f3f13b0d0272510f7e6f460fc0ca40ee1
Gdel-J <jessygdelvecchio@gmail.com>
2023-09-24 18:48:58 -0400

    mutation folder and clientMutation.js

--- client/src/components/ClientRow.js
+++ client/src/components/ClientRow.js
@@ -1,6 +1,24 @@
 import { FaTrash } from "react-icons/fa";
+import {from, useMutation} from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+IMPORT { GET_CLIENTS} from '..queries/clientQueries';
 
 export default function ClientRow({ client }) {
+
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+  variables: { id: client.id },
+  //refetchQueries: [{ query: GET_CLIENTS }],
+  update(cache, { data: { deleteClient'} }) {
+
+    const { clients } = cache.readQuery({ query:
+    GET_CLIENTS});
+    cache.writeQuery({
+
+      query: GET_CLIENTS,
+      data: { clients: clients.filter(client => client.id !== deleteClient.id)},
+    });
+  }
+  });
   return (
     <tr>
       <td>{client.name}</td>
@@ -8,7 +26,8 @@ export default function ClientRow({ client }) {
       <td>{client.phone}</td>
 
       <td>
-        <button className="btn.btn-danger btn-sm">
+        <button className='btn.btn-danger btn-sm'
+        onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -1,6 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
 import ClientRow from './ClientRow';
-import { GET_CLIENTS } from '../queries/clientQuerieS';
+import { GET_CLIENTS } from '../queries/clientQueries';
 import  Spinner from './Spinner';
 
 
