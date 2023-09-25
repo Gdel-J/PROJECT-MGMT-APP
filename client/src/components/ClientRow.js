7d290cf5b7786bb1acd60683a920e5ddbdf1057e
Gdel-J <jessygdelvecchio@gmail.com>
2023-09-24 20:07:59 -0400

    clientMutation.js file implemented, dlet client and add client mutation added

--- client/src/components/ClientRow.js
+++ client/src/components/ClientRow.js
@@ -1,33 +1,28 @@
 import { FaTrash } from "react-icons/fa";
-import {from, useMutation} from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
-IMPORT { GET_CLIENTS} from '..queries/clientQueries';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 export default function ClientRow({ client }) {
 
   const [deleteClient] = useMutation(DELETE_CLIENT, {
-  variables: { id: client.id },
-  //refetchQueries: [{ query: GET_CLIENTS }],
-  update(cache, { data: { deleteClient'} }) {
-
-    const { clients } = cache.readQuery({ query:
-    GET_CLIENTS});
-    cache.writeQuery({
-
-      query: GET_CLIENTS,
-      data: { clients: clients.filter(client => client.id !== deleteClient.id)},
-    });
-  }
+    variables: { id: client.id },
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter(clientItem => clientItem.id !== deleteClient.id) },
+      });
+    }
   });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
-
       <td>
-        <button className='btn.btn-danger btn-sm'
-        onClick={deleteClient}>
+        <button className='btn btn-danger btn-sm' onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
