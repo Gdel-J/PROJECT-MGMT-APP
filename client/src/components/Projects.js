54f9840bb1bc5c5c378637a5e5c60f8fd0fe20a0
Gdel-J <jessygdelvecchio@gmail.com>
2023-09-26 11:03:21 -0400

    pages folder and Home.js,NotFound.js andProject.js files created

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -12,7 +12,7 @@ export default function Projects() {
   return (
     <>
       {data.projects && data.projects.length > 0 ? (
-        <div className="row">
+        <div className="row mt-5">
           {data.projects.map((project) => ( 
             <ProjectCard key={project.id} project={project} /> 
           ))}
